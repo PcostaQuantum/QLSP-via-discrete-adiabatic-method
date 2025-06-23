4df29d362a6674602d6f84fe569a8b793e67799c
PcostaQuantum <85160765+PcostaQuantum@users.noreply.github.com>
2025-06-23 10:59:18 +1000

    Update Rot_sched.m

--- Rot_sched.m
+++ Rot_sched.m
@@ -1,7 +1,5 @@
 function [M]=Rot_sched(cond,p,s)
 
-%Rs rotation that depends on the condition number of the matrix A
-
 M=1/(sqrt((1-f(cond,p,s))^2+f(cond,p,s)^2))*[1-f(cond,p,s),f(cond,p,s);f(cond,p,s),-(1-f(cond,p,s))];
 
 end
